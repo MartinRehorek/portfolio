96ea79fce362e2316d23f9e10ea2184dbae50bd9
Martin Řehořek <martinrehorek5@gmail.com>
2022-06-17 10:39:27 +0200

    2nd commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// navbar
 const navMenu = document.getElementById('nav-menu'),
 navToggle = document.getElementById('nav-toggle'),
 navClose = document.getElementById('nav-close');
@@ -22,3 +23,42 @@ const linkAction = () => {
 }
 
 navLink.forEach( element => element.addEventListener('click', linkAction));
+
+//skills
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    if (itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open';
+    } else {
+        this.parentNode.className = 'skills__content skills__close';
+    }
+}
+
+skillsHeader.forEach( el => {
+    el.addEventListener('click', toggleSkills);
+})
+
+// qualifications
+
+const tabs = document.querySelectorAll('[data-target]'), tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach( tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+
+        tabs.forEach( tab => {
+            tab.classList.remove('qualification__active');
+        })
+
+        tab.classList.add('qualification__active');
+    })
+})
